66e3deba619f9f790dbad4704e13388ecfdef269
Mariia888 <bonya1907@gmail.com>
2022-11-23 17:39:35 +0200

    допрацювала 7 завдання

--- js/task-07.js
+++ js/task-07.js
@@ -4,3 +4,14 @@ const spanEl = document.querySelector('#text');
 console.log(inputEl);
 console.log(spanEl);
 
+
+inputEl.addEventListener("input", (event) => {
+ const size = inputEl.value;
+ spanEl.style.fontSize = size + "px";
+}); 
+
+//input.value {
+ //   spanEl.getElementsByClassName.fontSize = input.value + "px"
+//});
+
+
